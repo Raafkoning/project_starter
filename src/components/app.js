39a83c6d8f50fbc7a7136e3db169d931299a5830
Scott Van Horn <19svanhorn@go.dsdmail.net>
2019-07-10 11:15:16 -0600

    Second Commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio/portfolio-container';
+import PortfolioItem from './portfolio/portfolio-items';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Moment</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <PortfolioContainer />
       </div>
     );
   }
---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+import PortfolioItem from "./portfolio-items";
+
+export default class PortfolioContainer extends Component{
+    constructor(){
+        super();
+
+        console.log("Portfolio container has rendered");
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>Portfolio items go here updated...</h2>
+                <PortfolioItem />
+            </div>
+        )
+    }
+}
