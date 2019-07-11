b99d7874805f88e08ba72f50abb9491adf4c1161
Scott Van Horn <19svanhorn@go.dsdmail.net>
2019-07-11 09:30:48 -0600

    fourth commit, New navigation files

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import moment from "moment";
 
 import PortfolioContainer from './portfolio/portfolio-container';
-import PortfolioItem from './portfolio/portfolio-items';
+import NavigationContainer from './navigation/navigation-container';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>Moment</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
         <PortfolioContainer />
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,31 +1,61 @@
+
 import React, { Component } from "react";
 
 import PortfolioItem from "./portfolio-items";
 
-export default class PortfolioContainer extends Component{
-    constructor(){
-        super();
+export default class PortfolioContainer extends Component {
+  constructor() {
+    super();
 
-            this.state = {
-                pageTitle: "Welcome to my portfolio bitch"
-            }
-    }
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: [
+        { title: "Quip", category: "eCommerce" },
+        { title: "Eventbrite", category: "Scheduling" },
+        { title: "Ministry Safe", category: "Enterprise" },
+        { title: "SwingAway", category: "eCommerce" }
+      ]
+    };
 
-    portfolioitems(){
-        const data = ["Quip", "Eventbrite", "Ministry Safe", "SwingAway"];
+    this.handleFilter = this.handleFilter.bind(this);
+  }
 
-        return  data.map(item => {
-            return <PortfolioItem title={item} url={"google.com"}/>;
-        })
-    }
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
 
-    render(){
-        return(
-            <div>
-                <h2>{this.state.pageTitle}</h2>
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem title={item.title} url={"google.com"} />;
+    });
+  }
 
-                {this.portfolioitems()}
-            </div>
-        )
-    }
+  render() {
+      if (this.state.isLoading){
+          return <div>Loading...</div>;
+      }
+    
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
+
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
+
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
